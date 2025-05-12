8fb7c6fc56bf79e0738778dec16cc175b7684ec3
egigiffari <egisipayung3@gmail.com>
2025-05-12 13:53:13 +0700

    chore: variable

---
+++ basic/variable/simple_message.py
@@ -0,0 +1,5 @@
+message = "This message from then"
+print(message)
+
+message = "This message from future"
+print(message)
---
+++ basic/variable/variable.py
@@ -0,0 +1,14 @@
+# we've added a variable named `message`
+# every variable is connected to a `value`
+message = "Hello Python Crash Course reader!"
+print(message)
+
+# you can change the value of variable
+message = "Hello Python Crash Course World!"
+print(message)
+
+
+# naming variabel rules:
+# variable names can contain only `letters, numbers, and underscores`. `message_1` but `1_message`
+# spaces are not allowed. `greeting_message` but `greeting message`
+# variable names should be short but descriptive. `name` but `n`, `student_name` but `s_n`, `name_length` but `length_of_persons_name`
